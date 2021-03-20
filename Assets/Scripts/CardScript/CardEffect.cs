5b43239b9e4b5ecd069e6003a7ec81b481d17034
Masaya Heywood <mkh429@nyu.edu>
2021-03-20 05:37:11 -0400

    Further work on 3d class

--- Assets/Scripts/CardScript/CardEffect.cs
+++ Assets/Scripts/CardScript/CardEffect.cs
@@ -61,12 +61,12 @@ public class CardEffect : MonoBehaviour
 
     public void zz_Gain_Technician(int stack)
     {
-        //Services.playerBuffManager.GainNewBuff(new Technician(), stack);
+        Services.playerBuffManager.GainNewBuff(new Technician(), stack);
     }
 
     public void zz_Gain_Manager(int stack)
     {
-        //Services.playerBuffManager.GainNewBuff(new Technician(), stack);
+        Services.playerBuffManager.GainNewBuff(new Manager(), stack);
     }
 
     public void zz_On_Hold_Scenario(int stack)
