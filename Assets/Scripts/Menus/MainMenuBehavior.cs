84f68b7835c7e96db1f916a10a1da52830135808
Masaya Heywood <mkh429@nyu.edu>
2021-03-28 22:32:46 -0400

    Furthermenu Work

---
+++ Assets/Scripts/Menus/MainMenuBehavior.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuBehavior : MonoBehaviour
+{
+    public GameObject MainMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //MainMenuButton();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
