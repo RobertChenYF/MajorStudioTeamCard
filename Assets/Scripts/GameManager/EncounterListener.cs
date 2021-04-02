7b31208d0ddbe01c39d461f6e3a8f6f3da894581
Masaya Heywood <mkh429@nyu.edu>
2021-04-02 00:31:40 -0400

    Pre-Staging Work

---
+++ Assets/Scripts/GameManager/EncounterListener.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncounterListener : MonoBehaviour
+{
+    //Placeholder script for a future buffer script between the game manager and the encounter scene to make the transition from each less tightly coded
+}
