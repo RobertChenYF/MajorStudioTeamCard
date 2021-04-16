70a437318a5c2c0c3b33dd6bc507c9cc7aab3bf5
Zman <zes888@gmail.com>
2021-04-16 00:34:37 -0400

    Juicier Enemy Damage Effect

--- Assets/Scripts/EventSystem/VisualEffectManager.cs
+++ Assets/Scripts/EventSystem/VisualEffectManager.cs
@@ -6,21 +6,25 @@ using UnityEngine;
 
 public class VisualEffectManager : MonoBehaviour
 {
-
+    [Header ("Enemy Damage Particle Effect")]
     [SerializeField] private TextMeshPro DamageTextPrefab;
     [SerializeField] private Vector3 damageParticleOffset;
     [SerializeField] private Vector3 damageParticleEndOffset;
     [SerializeField] private float damageParticleTime;
-    [SerializeField] private float flashTime;
-    [SerializeField] private Color dmgFlashColor;
-    [SerializeField] private Canvas canvas;
     [SerializeField] private float particleTransformSmoothing;
     [SerializeField] private float particleColorSmoothing;
     [SerializeField] private Color dmgParticleColor;
+    [SerializeField] private Vector3 jitterOffset;
+    [SerializeField] private float jitterSmoothing;
+
+    [Header ("Flash Effect")]
+    [SerializeField] private Color dmgFlashColor;
+    [SerializeField] private float flashSmoothingIn;
+    [SerializeField] private float flashSmoothingOut;
 
     [Header ("Audio Effect Settings")]
     [SerializeField] private AudioSource audioSource;
-    [SerializeField] private AudioClip dmgSound;
+    [SerializeField] private AudioClip enemyDmgSound;
     [SerializeField] private float dmgSoundVolume;
     [SerializeField] private AudioClip playerDmgSound;
     [SerializeField] private float playerDmgSoundVolume;
@@ -28,7 +32,7 @@ public class VisualEffectManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        audioSource = this.gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -43,9 +47,9 @@ public class VisualEffectManager : MonoBehaviour
 
     }
 
-    void PlayDamgeSound()
+    void PlayEnemyDamageSound()
     {
-        audioSource.clip = dmgSound;
+        audioSource.clip = enemyDmgSound;
         audioSource.volume = dmgSoundVolume;
         audioSource.Play();
     }
@@ -59,10 +63,50 @@ public class VisualEffectManager : MonoBehaviour
 
     IEnumerator PlayFlashEffect(SpriteRenderer sr, Color color)
     {
-        //lerp the sprite color to hue
+        Color temp = sr.color;
 
+        //print("Beginning Enemy Damage Flash");
+
+        //lerp the sprite color to hue
+        while (Mathf.Abs(color.r - sr.color.r) > 0.05f || Mathf.Abs(color.g - sr.color.g) > 0.05f || Mathf.Abs(color.b - sr.color.b) > 0.05f) //use red value for tracking change
+        {
+            sr.color = Color.Lerp(sr.color, color, flashSmoothingIn * Time.deltaTime);
+            //print("Current Color: " + sr.color.ToString());
+
+            yield return null;
+        }
+
+        //lerp back
+        while (Mathf.Abs(color.r - sr.color.r) > 0.05f || Mathf.Abs(color.g - sr.color.g) > 0.05f || Mathf.Abs(color.b - sr.color.b) > 0.05f) //use red value for tracking change
+        {
+            sr.color = Color.Lerp(sr.color, temp, flashSmoothingOut * Time.deltaTime);
+
+            yield return null;
+        }
+
+        sr.color = temp;
 
         yield return null;
+    }
+
+    IEnumerator PlayEnemyHitJitter(Enemy enemy)
+    {
+
+        Vector3 tempPos = enemy.gameObject.transform.position;
+
+        while (Vector3.Distance(enemy.gameObject.transform.position, tempPos + jitterOffset) > 0.05f)
+        {
+            enemy.gameObject.transform.position =
+                Vector3.Slerp(enemy.gameObject.transform.position, tempPos + jitterOffset, jitterSmoothing * Time.deltaTime);
+
+            print("POS: " + enemy.gameObject.transform.position.ToString());
+
+            yield return null;
+        }
+
+        enemy.gameObject.transform.position = tempPos;
+
+            yield return null;
     }
 
     //Damage Number Throw Out Effect
@@ -70,33 +114,32 @@ public class VisualEffectManager : MonoBehaviour
     {
         enemy.is_Idle = false;
 
-        print("Starting Enemy Damage Effect.");
+        //print("Starting Enemy Damage Effect.");
 
         //Create the particle at the enemy location with offset and set text
         TextMeshPro dmgTxt = Instantiate(DamageTextPrefab);
         dmgTxt.gameObject.transform.position = enemy.gameObject.transform.position + damageParticleOffset;
         dmgTxt.text = "" + damage.ToString();
-
         float currentAlpha = 1;
 
+        //Play Flash, jitter, and Sound EFfect
         StartCoroutine(PlayFlashEffect(enemy.gameObject.GetComponent<SpriteRenderer>(), dmgFlashColor));
-
-        PlayDamgeSound();
+        StartCoroutine(PlayEnemyHitJitter(enemy));
+        PlayEnemyDamageSound();
 
         //lerp the position of the text particle while fading out
         while (Vector3.Distance(dmgTxt.gameObject.transform.position, enemy.gameObject.transform.position + damageParticleEndOffset) > 0.05f)
         {
-            dmgTxt.gameObject.transform.position = 
+            dmgTxt.gameObject.transform.position = //Slerp position
                 Vector3.Slerp(dmgTxt.gameObject.transform.position, enemy.gameObject.transform.position + damageParticleEndOffset, particleTransformSmoothing * Time.deltaTime);
-            currentAlpha = Mathf.Lerp(currentAlpha, 0f, particleColorSmoothing * Time.deltaTime);
-            dmgTxt.faceColor = new Color(dmgParticleColor.r, dmgParticleColor.g, dmgParticleColor.b, currentAlpha); //fade out text before deletion
 
-            print("Alpha: " + currentAlpha);
+            currentAlpha = Mathf.Lerp(currentAlpha, 0f, particleColorSmoothing * Time.deltaTime);
+            dmgTxt.faceColor = new Color(dmgParticleColor.r, dmgParticleColor.g, dmgParticleColor.b, currentAlpha); //fade out text
 
             yield return null;
         }
 
-        print("Particle Destroyed");
+        //print("Particle Destroyed");
         Destroy(dmgTxt);
 
         enemy.is_Idle = true;
